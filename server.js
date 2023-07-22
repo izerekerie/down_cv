0f7a730475fbfd5e22f2718c5bf24700668d6957
izereuwonkunda <izereuwonkundakelia@gmail.com>
2023-07-22 22:11:54 +0200

    send-email

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const fs = require('fs');
 const path = require('path');
-const PORT = 5000; // You can choose any available port
+const nodemailer = require('nodemailer');
 
+const PORT = 5000; // You can choose any available port
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
 app.use(cors());
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'your_email@gmail.com', // Replace with your email address
+    pass: 'your_email_password', // Replace with your email password or app-specific password
+  },
+});
 
 app.get('/download-cv', (req, res) => {
   const cvFilePath = path.join(__dirname, 'cv', 'cv.pdf');
@@ -22,6 +31,29 @@ app.get('/download-cv', (req, res) => {
   cvFileStream.pipe(res);
 
 });
+
+app.post('/send-email', (req, res) => {
+  console.log("here")
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email, // Replace with your email address
+    to: 'izereuwonkundakelia@gmail.com', // Replace with the recipient's email address
+    subject: 'New message from your portfolio',
+    text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
